c79ffa0f808bbc5acb7b02a2a69a2faf3f972792
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-01-07 00:23:24 +0530

    Nextjs 13 with CCA 🌠

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-
-// import { Inter } from "@next/font/google";
-
-// const inter = Inter({
-//   subsets: ["latin"],
-// });
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: [inter.className, ...defaultTheme.fontFamily.sans],
-  //     },
-  //   },
-  // },
+  darkMode: "class",
+  content: ["components/**/*.{js,jsx,ts,tsx}", "pages/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          damp: "#6674D9",
+        },
+        gray: {
+          bg: "#1A202C",
+          lightbg: "#2D3748",
+        },
+        primary: {
+          50: "#e5e7f9",
+          100: "#bec4ef",
+          200: "#929ce4",
+          300: "#6674d9",
+          400: "#4657d1",
+          500: "#2539c9",
+          600: "#2133c3",
+          700: "#1b2cbc",
+          800: "#1624b5",
+          900: "#0d17a9",
+        },
+      },
+    },
+  },
   plugins: [
-    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("tailwind-scrollbar-hide"),
   ],
 };
